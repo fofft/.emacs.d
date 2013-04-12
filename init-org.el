3af99481d937d14316e13930424f0640a2217be1
Wyatt Lee Chastain <wyatt@fofft.com>
2013-04-12 15:33:37 -0500

    org-mode stuff added/changed

--- init-org.el
+++ init-org.el
@@ -1,16 +1,23 @@
+(setq folderPath  (format "~/.emacs.d/org/%s"
+			  (format-time-string "%Y/%m (%B)/"))
+      fileName    (format "%s-%s-%s %s.org"
+			  (format-time-string "%Y")
+			  (format-time-string "%m")
+			  (format-time-string "%d")
+			  (format-time-string "%A")))
+(setq fullFileName (concat folderPath fileName))
+
 (defun start-log ()
+  "Creates a new org file with today's date for new tasks"
   (interactive)
-  (setq folderPath  (format "~/.emacs.d/org/%s"
-			    (format-time-string "%Y/%m (%B)/"))
-	fileName    (format "%s-%s-%s %s.org"
-			 (format-time-string "%Y")
-			 (format-time-string "%m")
-			 (format-time-string "%d")
-			 (format-time-string "%A")))
-  (setq fullFileName (concat folderPath fileName))
   ;; TODO: Put in check and create new year / month folders if they don't exist.
   (if (not (file-exists-p fullFileName))
       (write-region (format-time-string "%Y-%m-%d %A %T") nil fullFileName))
   (find-file fullFileName))
+
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "|" "DONE(d)") ;; general
+	(sequence "ASSIGNED(a)" "FEEDBACK(f)" "|" "RESOLVED(r)") ;; work
+	))
 
 (provide 'init-org)
