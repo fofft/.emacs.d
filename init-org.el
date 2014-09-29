6ec5ac912484d93e1313f661f32aba13a5d46728
Wyatt Lee Chastain <wyatt.lee.chastain@gmail.com>
2014-09-29 17:16:23 -0500

    Minor changes
    Small change to add a todo list function and remove code that I wasn't
    using.

--- init-org.el
+++ init-org.el
@@ -2,29 +2,9 @@
 
 (setq org-agenda-files (list "~/.emacs.d/org/"))
 
-(defun daily-log-file-name ()
-  "For daily log files, returns file name with today's date."
-  (setq folderPath  (format "~/.emacs.d/org/%s"
-			    (format-time-string "%Y/%m (%B)/"))
-	fileName    (format "%s-%s-%s %s.org"
-			    (format-time-string "%Y")
-			    (format-time-string "%m")
-			    (format-time-string "%d")
-			    (format-time-string "%A")))
-  (concat folderPath fileName))
-
-(defun start-log ()
-  "Creates a new org file with today's date for new tasks"
-  (interactive)
-  ;; TODO: Put in check and create new year / month folders if they don't exist.
-  (setq fullFileName (funcall 'daily-log-file-name))
-  (if (not (file-exists-p fullFileName))
-      (write-region (format-time-string "%Y-%m-%d %A %T\n\n") nil fullFileName))
-  (find-file fullFileName))
-
 (setq org-todo-keywords
       '((sequence "TODO(t)" "|" "DONE(d)") ;; general
-	(sequence "ASSIGNED(a)" "FEEDBACK(f)" "|" "RESOLVED(r)") ;; work
+	(sequence "ASSIGNED(a)" "FEEDBACK(f)" "|" "RESOLVED(r)" "RETURNED(e)") ;; work
 	))
 
 (setq org-goto-interface 'outline-path-completion
@@ -38,5 +18,13 @@
     (org-back-to-heading)
     (org-add-note)))
 (add-hook 'org-clock-out-hook 'check-for-clock-out-note)
+
+(defun todo()
+  (interactive)
+  (find-file (concat local-dropbox "TODO.org")))
+
+(defun todo-local()
+  (interactive)
+  (find-file (concat local-desktop "TODO.org")))
 
 (provide 'init-org)
--- init.el
+++ init.el
@@ -1,8 +1,8 @@
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/"))
 
 (require 'init-packages)
+(require 'init-local)
 (require 'init-org)
-(require 'init-twit)
 
 ;;smex settings
 (global-set-key (kbd "M-x") 'smex)
@@ -57,3 +57,18 @@
       kept-new-versions 20  ; how many of the newest versions to keep
       kept-old-versions 5   ; and how many of the old
 )
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "9f443833deb3412a34d2d2c912247349d4bd1b09e0f5eaba11a3ea7872892000" default)))
+ '(ispell-dictionary "english")
+ '(ispell-program-name windows-ispell-dictionary))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
