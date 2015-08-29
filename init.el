6264c9b1e45cdca649ff75c6871da95feedd4dd3
Wyatt Lee Chastain <wyatt.lee.chastain@gmail.com>
2015-08-29 16:46:54 -0500

    Move custom variables to own file.

--- init.el
+++ init.el
@@ -75,21 +75,8 @@
   (insert "\n")
   )
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("28ec8ccf6190f6a73812df9bc91df54ce1d6132f18b4c8fcc85d45298569eb53" default)))
- '(package-selected-packages (quote (batch-mode smex))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(setq custom-file "~/.emacs.d/lisp/init-custom.el")
+(load custom-file)
 
 ;; Load theme
 (load-theme 'afternoon)
